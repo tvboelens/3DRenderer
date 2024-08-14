9ca017679edf3975f447f2732833bab4608b8f3f
tvboelens <tvboelens@gmail.com>
2024-08-14 17:35:42 +0200

    Constructor was missing

--- src/Raytracer/raytracer.h
+++ src/Raytracer/raytracer.h
@@ -14,7 +14,15 @@ class RayTracer
     double viewport_width;
     double viewport_height;
 
-    vec3 CanvasToViewport(int x, int y, const Canvas& C);
+    RayTracer(const vec3 &camera_position_ = {0.0, 0.0, 0.0},
+              double viewport_distance_ = 1.0,
+              double viewport_width_ = 1.0,
+              double viewport_height_ = 1.0) 
+              : camera_position{camera_position_}
+              , viewport_distance{viewport_distance_}
+              , viewport_height{viewport_height_}
+              , viewport_width{viewport_width_} {};
+    vec3 CanvasToViewport(int x, int y, const Canvas &C);
     Color TraceRay(const Ray &ray, const Scene& scene, double t_min, double t_max);
     void PaintCanvas(Canvas C, const Scene &S);
 };
