da26c710c67e07896cdca834277a5c4d5cb47862
tvboelens <tvboelens@gmail.com>
2024-08-16 11:27:37 +0200

    Render scene with lights

--- src/Raytracer/main.cc
+++ src/Raytracer/main.cc
@@ -3,6 +3,7 @@
 #include "../scene.h"
 #include "../sphere.h"
 #include "../color.h"
+#include "light.h"
 
 int main()
 {
@@ -14,6 +15,11 @@ int main()
     scene.addSphere(Sphere{{0.0, -1.0, 3.0}, 1.0, {255.0, 0.0, 0.0}}); // Red
     scene.addSphere(Sphere{{2.0, 0.0, 4.0}, 1.0, {0.0, 0.0, 255.0}}); // Blue
     scene.addSphere(Sphere{{-2.0, 0.0, 4.0}, 1.0, {0.0, 255.0, 0.0}}); // Green
+    scene.addSphere(Sphere({0.0,-5001.0,0.0}, 5000.0, {255.0,255.0,0.0}));//Yellow
+
+    scene.addLight(Light(ambient, 0.2));
+    scene.addLight(Light(point, 0.6, {2, 1, 0}));
+    scene.addLight(Light(directional, 0.2, {1, 4, 4}));
 
     raytracer.PaintCanvas(canvas, scene);
     canvas.render();
